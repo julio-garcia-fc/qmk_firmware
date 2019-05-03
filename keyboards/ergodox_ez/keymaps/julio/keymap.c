bfb7e0ad443387a5cd003969bbeec800b79be7d9
Garcia, Julio C <jgarcia@digitalocean.com>
2019-05-03 09:55:03 -0600

    Minor whitespace cleanup

--- keyboards/ergodox_ez/keymaps/julio/keymap.c
+++ keyboards/ergodox_ez/keymaps/julio/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                    KC_LCMD,KC_LALT,
                                                                                            KC_HOME,
                                                                             KC_SPC,KC_ENT, KC_END,
-        
+ 
             KC_RGHT, KC_6,KC_7, KC_8,   KC_9,   KC_0,            KC_MINS,
             TG(SYMB),KC_Y,KC_U, KC_I,   KC_O,   KC_P,            KC_BSLS,
                      KC_H,KC_J, KC_K,   KC_L,   LT(MDIA,KC_SCLN),RALT_T(KC_QUOT),
@@ -57,7 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                 RGB_MOD,_______,
                                                                                 _______,
                                                                 RGB_VAD,RGB_VAI,_______,
-        
 
             _______,KC_F6,  KC_F7,  KC_F8,  KC_F9,KC_F10, KC_F11,
             _______,KC_UP,  KC_7,   KC_8,   KC_9, KC_ASTR,KC_F12,
@@ -78,8 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                    _______,_______,
                                                                                    _______,
                                                                    _______,_______,_______,
-        
-        
+
             _______,_______,_______,_______,_______,_______,_______,
             _______,_______,_______,_______,_______,_______,_______,
                     _______,_______,_______,_______,_______,KC_MPLY,
