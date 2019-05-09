4c1041cbaffd30d3f839c81ebc44525c3e4b341c
Garcia, Julio C <jgarcia@digitalocean.com>
2019-05-09 11:20:18 -0600

    Remove enter from the right hand

--- keyboards/ergodox_ez/keymaps/julio/keymap.c
+++ keyboards/ergodox_ez/keymaps/julio/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,          KC_1,        KC_2,         KC_3,      KC_4,   KC_5,KC_ESC,
         KC_TAB,          KC_Q,        KC_W,         KC_E,      KC_R,   KC_T,TG(SYMB),
         LALT_T(KC_APP),  KC_A,        KC_S,         KC_D,      KC_F,   KC_G,
-        LSFT_T(KC_ENTER),LCTL_T(KC_Z),KC_X,         KC_C,      KC_V,   KC_B,KC_SPC,
+        KC_LSFT,         LCTL_T(KC_Z),KC_X,         KC_C,      KC_V,   KC_B,KC_SPC,
         LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT),KC_LEFT,   KC_RGHT,
                                                                                    KC_LCMD,KC_LALT,
                                                                                            KC_HOME,
